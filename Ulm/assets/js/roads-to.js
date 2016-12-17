095ca6adad0be1165ca44cba4225efd0074a4d8f
Alexey Valikov <alexey.valikov@deutschebahn.com>
2016-12-17 01:58:45 +0100

    Web.

---
+++ Ulm/assets/js/roads-to.js
@@ -0,0 +1,22 @@
+$(function() {
+
+	// Setup leaflet map
+	var map = new L.Map('map');
+
+	var basemapLayer = new L.TileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoib3JsZXNzIiwiYSI6ImNpaXg4cGt2cTAwMGh2Mm01ZDlqYnk5N28ifQ.EDKxdytV0xTiHyI16K0zsg',
+			{attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery &copy; <a href="http://mapbox.com">Mapbox</a>',
+	id: 'mapbox.streets'
+
+});
+
+	// Center map and default zoom level
+	map.setView([48.399368824050626, 9.984123929980187], 13);
+
+	// Adds the background layer to the map
+	map.addLayer(basemapLayer);
+
+	L.geoJSON(roads).addTo(map);
+
+});
